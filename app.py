6bc4b1b5f587d00797d1697d3a758e3ccb85af36
JohanKEAe2020 <74414088+JohanKEAe2020@users.noreply.github.com>
2021-06-03 16:31:36 +0200

    Email final 2.3

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ import smtplib, ssl
 @app.route("/")
 def advarselsemail():
     
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
-    sender_email = "Keagruppe6A@gmail.com"  # Enter your address
-    receiver_email = "Keagruppe6A@gmail.com"  # Enter receiver address
-    password = "NaturCenterAmagerStrand" #input("Type your password and press enter: ")
+    port = 465                                      #Email port           
+    smtp_server = "smtp.gmail.com"                  #SMTP-server
+    sender_email = "Keagruppe6A@gmail.com"          #Sender Email
+    receiver_email = "Keagruppe6A@gmail.com"        #Receiver Email
+    password = "NaturCenterAmagerStrand"            #Password to sender Email
 
-    name="Thomas"
-    antal=75
+    name="Thomas"                                   #Variabler til Emailens indhold
+    antal=75                                        #Skal indtastes manuel
     tid=ctime()
     subject="Dagens advarsel"
 
-    message = 'Subject: {}\n\n'.format(subject)
+    message = 'Subject: {}\n\n'.format(subject)     #Email strengen
     message+='Hej {}, \n\n'.format(name)
     message+='Antallet af mennsker var over: {} \n'.format(antal)
     message+='Tidspunkt: {} \n\n'.format(tid)
     message+="Venlig hilsen RPI."
 
-    context = ssl.create_default_context()
+    context = ssl.create_default_context()          #Oprettelse af SSL
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
-    return "Advarsel afsendt"
+        server.login(sender_email, password)        
+        server.sendmail(sender_email, receiver_email, message)  #Afsendelse af email
+    return "Advarsel afsendt"                       #Webside skrift
